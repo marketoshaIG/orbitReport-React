f029eb26ad5315b067f71f3167c6c5d35c7ff18e
marketoshaIG <137831388+marketoshaIG@users.noreply.github.com>
2023-08-09 11:05:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React, { useState } from "react";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
-
+import Banner from "./components/Banner";
+import satData from "./components/satData";
+ 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(filteredSats);
+  };
+
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    <>
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
+    </>
   );
 }
+
 
 export default App;
