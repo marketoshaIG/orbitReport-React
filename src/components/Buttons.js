081337da82feb9b4212a85bfd98ee0d32a9842e6
marketoshaIG <137831388+marketoshaIG@users.noreply.github.com>
2023-08-11 17:06:59 -0500

    it just gets better

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,18 +2,20 @@ import satData from "./satData";
 import './styling.css'
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const handleSatClick = (sat) => {
+    filterByType(sat);
+  };
+
   return (
     <div className="flex-container">
-      {displaySats.map((sat, id) => {
-        return (
-          <button
-            onClick={() => filterByType(sat)}
-            key={id}
-          >
-            {sat} Orbit
-          </button>
-        );
-      })}
+      {displaySats.map((sat) => (
+        <button
+          onClick={() => handleSatClick(sat)}
+          key={sat}
+        >
+          {sat} Orbit
+        </button>
+      ))}
       <button onClick={() => setSat(satData)}>
         All Orbits
       </button>
